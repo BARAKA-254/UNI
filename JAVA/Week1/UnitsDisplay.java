fcce7d8ae50f86cc141aac7e6679ed6f866759ed
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-18 10:38:05 +0300

    UNIT DISPLAY PROGRAM

---
+++ JAVA/Week1/UnitsDisplay.java
@@ -0,0 +1,9 @@
+public class UnitsDisplay{
+    public static void main(String[] args){
+        System.out.println("1. Introductio to Programming. ");
+        System.out.println("2. Data Structures and Algorithms. ");
+        System.out.println("3. Computer Networks. ");
+        System.out.println("4. Database Management Systems. ");
+        System.out.println("5. Software Engineering. ");
+    }
+}
